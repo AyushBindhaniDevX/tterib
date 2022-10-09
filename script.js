ee3761135617bf81a59a93062c6696405dd60f88
Ayush Bindhani <73215402+AyushBindhaniDevX@users.noreply.github.com>
2022-10-09 15:42:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 feather.replace()
+fetch("https://api.thingspeak.com/channels/1887074/feeds.json?results=1").then((data) => {
+  return data.json();
+
+}).then((objectData) => {
+  console.log(objectData.feeds[0].field1);
+  var output = document.getElementById('test');
+  output.innerHTML = objectData.feeds[0].field1;
+});
+
